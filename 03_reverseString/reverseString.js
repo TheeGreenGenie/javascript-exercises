f08ed5f051a6f4ff2a3cf3e796acc0bb4bdba5b5
theegreengenie <solomonshasanmi@gmail.com>
2024-06-18 19:01:27 -0400

    Writing Function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    str = str.split('');
+    str = str.reverse();
+    str = str.join('');
+    return str;
 };
 
 // Do not edit below this line
