cac3ba550cc3e19936d547ba830041cf228dadbf
theegreengenie <solomonshasanmi@gmail.com>
2024-06-18 18:51:49 -0400

    fixed to pass test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(str, repetitions) {
     let term = '';
+    if (repetitions < 0) {
+        return 'ERROR';
+    }
     while (repetitions > 0) {
         term+=str;
         repetitions--;
