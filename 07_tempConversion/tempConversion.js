8b04a44acefeae3afb9ea730535332a10ccb3096
theegreengenie <solomonshasanmi@gmail.com>
2024-06-26 21:15:40 -0400

    tempConversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFah) {
+  let tempCel = (tempFah - 32) * (5/9)
+  return +tempCel.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  let tempFah = (tempCel * (9/5))+32;
+  return +tempFah.toFixed(1)
 };
 
 // Do not edit below this line
