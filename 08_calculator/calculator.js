3067f410ee4c65734aacc8e1e76b627a6f046960
theegreengenie <solomonshasanmi@gmail.com>
2024-07-06 19:50:52 -0400

    calculator update

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((current, value) => current+Number(value), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((current, value) => current*Number(value), 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let arr = []
+	for (let i=num; i > 0; i--) {
+    arr.push(i)
+  }
+  return arr.reduce((current, value) => current*value, 1)
 };
 
 // Do not edit below this line
