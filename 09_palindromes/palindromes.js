aaf1520fdffed020841a635b3c033e24cb10bc16
theegreengenie <solomonshasanmi@gmail.com>
2024-07-06 20:04:09 -0400

    palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    newStr = str.split('').reverse().join('');
+    return (str === newStr);
 };
 
 // Do not edit below this line
