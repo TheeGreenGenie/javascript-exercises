636fc9d1db65a83035f26ff3ee952809339740e5
theegreengenie <solomonshasanmi@gmail.com>
2024-07-07 12:18:20 -0400

    fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const arr = []
+    if (num < 0) {
+        return 'OOPS'
+    }
+    if (num == 0) {
+        return 0
+    }
+    for (let i = num; i > 0; i--) {
+        let num = 1
+        if (arr.length < 2) {
+            arr.push(num)
+        } else {
+            num = Number(arr[arr.length-1])+Number(arr[arr.length-2])
+            arr.push(num)
+        }
+    }
+    return arr[arr.length-1]
 };
 
 // Do not edit below this line
