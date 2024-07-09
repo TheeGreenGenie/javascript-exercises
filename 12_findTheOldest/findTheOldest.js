b6731f3d7f827af4149d90ac1d5f6874772d74c0
theegreengenie <solomonshasanmi@gmail.com>
2024-07-08 20:46:01 -0400

    FINALLY DONE OH MY LORD

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let now = new Date().getFullYear(),
+    newArr = {name: 'N/a', age: 0};
+    arr.forEach(element => {
+        let ourName = element.name,
+            birth = element.yearOfBirth,
+            death = element.hasOwnProperty('yearOfDeath')? element.yearOfDeath : now,
+            ourAge = death-birth;
+        if (ourAge > newArr.age) {
+            newArr = {name: ourName, age: ourAge};
+        } 
+    });
+    return newArr;
 };
 
 // Do not edit below this line
